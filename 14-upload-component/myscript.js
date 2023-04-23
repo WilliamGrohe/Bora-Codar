641e1c96d21216b7915514164bf378ad9aca3818
William Grohe <william.grohe@gmail.com>
2023-04-22 23:32:41 -0300

    Adicionando nova box com dados do arquivo

--- 14-upload-component/myscript.js
+++ 14-upload-component/myscript.js
@@ -1,21 +1,59 @@
-const dropArea = document.querySelector('.upload')
-const box = document.querySelector('.box')
+const dropArea = document.querySelector(".upload");
+const box = document.querySelector(".box");
+const fileInput = document.querySelector("#file-input");
+const filesContainer = document.querySelector('.files')
 
-dropArea.addEventListener('dragover', ()=>{
-  dropArea.classList.add('upload-over')
-})
+fileInput.addEventListener("change", createNewBoxOnFiles);
 
-dropArea.addEventListener('dragleave', ()=>{
-  dropArea.classList.remove('upload-over')
-})
+function createNewBoxOnFiles() {
+  const file = fileInput.files[0];
+  const fileName = file.name;
+  const fileSize = parseFloat(file.size / (1024 * 1024)).toFixed(2); // convertendo para MB
 
-box.addEventListener('click', removeBox)
-function removeBox(event) {
-  if(event.target.classList.contains('removeBoxBtn')) {
-    const box = event.target.closest('.box')
-    box.classList.add('box-deleted')
-    setTimeout(() => {
-      box.remove()
-    }, 300)
+  const newBox = document.createElement("div");
+
+  newBox.classList.add("box");
+
+  newBox.innerHTML = `
+  <div class="icon">
+            <img src="Assets/file-uploading.svg" alt="File Icon">
+          </div>
+          <div class="info">
+            <div class="filename">${fileName}</div>
+            <div class="filesize">
+              <span class="loaded"></span>
+              <span class="totalSize">${fileSize}MB</span>
+            </div>
+            <div class="bar">
+              <progress value="45" max="100"></progress>
+              <span class="loadingPorcentage">46%</span>
+            </div>
+          </div>
+          <div class="action">
+            <img src="Assets/Cancel.svg" class="removeBoxBtn" alt="Cancel Icon">
+          </div>
+  `;
+
+  filesContainer.appendChild(newBox)
 }
+
+dropArea.addEventListener("dragover", () => {
+  dropArea.classList.add("upload-over");
+});
+
+dropArea.addEventListener("dragleave", () => {
+  dropArea.classList.remove("upload-over");
+});
+
+box.addEventListener("click", removeBox);
+
+
+function removeBox(event) {
+  if (event.target.classList.contains("removeBoxBtn")) {
+    const box = event.target.closest(".box");
+    box.classList.add("box-deleted");
+    setTimeout(() => {
+      box.remove();
+    }, 300);
+  }
 }
